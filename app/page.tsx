7d56028596377d0cac70b489fee1763a1dfa1f3e
Nikolas-Vardeberg <post@nikolas.no>
2024-04-14 21:18:47 +0200

    feat: Add admin and booking pages with Button UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import { Button, buttonVariants } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100'>
-      <Link href="/admin/signin">
-        Gå Til admin
+    <div className='flex items-center justify-center min-h-screen py-2 bg-gray-100 space-x-10'>
+      <Link className={buttonVariants({  size: "default", className: "max-md:hidden" })}
+        href="/admin" target="_blank"
+      >
+        Admin <ArrowRight className="ml-2 h-5 w-5" />
+      </Link>
+      <Link className={buttonVariants({ variant: "outline",  size: "default", className: "max-md:hidden" })}
+        href="/booking" target="_blank"
+      >
+        Booking <ArrowRight className="ml-2 h-5 w-5" />
       </Link>
     </div>
   )
